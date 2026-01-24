90b0c47a836cb7c3d838db410cd2fa39ac308384
OrAxelerator <axel.piat@icloud.com>
2026-01-24 12:30:45 +0100

    all the func to clone are here but not call

---
+++ pixel_code/script/git/clone.py
@@ -0,0 +1,22 @@
+import os
+import sys
+from subprocess import run
+
+
+sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.dirname(__file__))))
+from script.name import is_dir_empty
+from script.git.repo_name import get_name_repo
+from script.delete import delete_dir
+        
+def clone_repo(url):
+    name = get_name_repo(url)
+    if is_dir_empty(name):
+        if not delete_dir(name):
+            return False 
+            
+    run(["git", "clone", url], check=True)
+    return True
+
+
+#url = "https://github.com/OrAxelerator/trophee-nsi-ant.git" # start to pass / {name} = 19
+#clone_repo(url)
