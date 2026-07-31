1de7a3c4433faa6e1b91bb6ddb909a18a3516f44
OrAxelerator <axel.piat@icloud.com>
2026-07-31 18:41:13 +0200

    Add securty while fetching github data

--- update.py
+++ update.py
@@ -22,14 +22,24 @@ def extract_zip(zip_path, extract_dir):
         z.extractall(extract_dir)
 
 
-def get_latest_version():
-    """ can return release and pre-release"""
-    r = requests.get(URL)
-    releases = r.json()
+def get_latest_version()-> object | None:
+    """ 
+    can return release and pre-release.
+    And None if 404 or user don't have connection
+    """
 
+    try:
+        r = requests.get(URL)
+
+        if r.status_code == 404:
+            return None
+        else:
+            releases = r.json()
+    except:
+        return None
+    
     # print(releases[0]["tag_name"])      # la plus récente, même si c'est une prerelease
     # print(releases[0]["prerelease"])    # True ou False
-
     return releases[0]
 
 
